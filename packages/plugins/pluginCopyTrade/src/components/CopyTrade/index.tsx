6db0d54a7a96b7982f462246ee9353e7249957c0
dinhnt97 <163962843+dinhnt97@users.noreply.github.com>
2025-01-18 21:47:09 +0700

    u code

--- packages/plugins/pluginCopyTrade/src/components/CopyTrade/index.tsx
+++ packages/plugins/pluginCopyTrade/src/components/CopyTrade/index.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import { useWallet } from '@coin98-com/wallet-adapter-react';
-import { Root, TabsContent, TabsList, TabsTrigger } from '@radix-ui/react-tabs';
 import { Button } from '@repo/ui';
 import React, { useEffect } from 'react';
 import BaseAPI from '../../axios';
 import { useCheckHub } from '../../context/checkHubContext';
+import { TabsList, Root, TabsTrigger, TabsContent } from '@radix-ui/react-tabs';
 import { handleListenEvent } from '../../hooks/useEmitEvents';
+import { tokenOut } from '../../utils/constants';
 
 const CopyTraderItem = () => {
   const { onGetPrivateKey } = useCheckHub();
@@ -61,7 +62,7 @@ const CopyTrade = () => {
   };
 
   useEffect(() => {
-    BaseAPI({ method: 'GET', url: '/trades' }).then((res) => {
+    BaseAPI({ method: 'GET', url: '/ping' }).then((res) => {
       console.log(res);
     });
     if (!privateKey) {
@@ -89,9 +90,73 @@ const CopyTrade = () => {
             ))}
           </div>
         </TabsContent>
-        <TabsContent value="history">{'history'}</TabsContent>
+        <TabsContent value="history">
+          {'history'}
+          </TabsContent>
       </Root>
     </div>
+  );
+};
+
+const TradingHistoryItem = ({item}:{item:any}) => {
+  return (
+    <div/>
+  )
+}
+
+type ITradingHistoryItem = {
+  amountIn: string;
+  amountInUsd: string;
+  amountOut: string;
+  amountOutUsd: string;
+  tokenIn:{
+    address: string;
+    symbol: string;
+    decimals: number;
+  },
+  tokenOut:{
+    address: string;
+    symbol: string;
+    decimals: number;
+  },
+}
+
+const TradingHistory = () => {
+
+  const [tradingHistories, setTradingHistories] = React.useState<ITradingHistoryItem[]>([]);
+
+  const onExecuteTrade = async (event:any) => {
+    BaseAPI({ method: 'POST', url: '/swap', payload:{
+      'tokenIn': {
+        'address': '0xc55e93c62874d8100dbd2dfe307edc1036ad5434',
+        'decimals': 18
+      },
+      'tokenOut': {
+          'address': '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913',
+          'decimals': 6
+      },
+      'amountIn': 200,
+      'senderAddress': '0x09D0A2963D27B27C234b3637C528eCB9356B8867'
+    }}).then((res) => {
+      console.log(res);
+    });
+  }
+
+  useEffect(()=>{
+    handleListenEvent('address', onExecuteTrade);
+    //listen socket info here
+    
+  },[])
+
+  return (
+    <>
+      <div className="text-xl font-bold mb-4">Trader List:</div>
+        <div className="flex flex-col gap-4">
+          {tradingHistories.map((item) => (
+            <TradingHistoryItem key={item} />
+          ))}
+      </div>
+    </>
   );
 };
 
